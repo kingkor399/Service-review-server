a3f35923ad7b21897a952547ffa97ab4db33210a
kingkor399 <farhanrahmanyou@gmail.com>
2022-11-09 19:45:30 -0800

    fourth commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -27,6 +27,19 @@ async function run(){
             res.send(services);
         })
 
+        app.get('/allservice',  async(req, res)=>{
+            const query = {};
+            const cursor = photograpyCollection.find(query);
+            const allservice = await  cursor.toArray();
+            res.send(allservice);
+        })
+
+        app.get('/servicedetails/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await photograpyCollection.findOne(query);
+            res.send(service);
+        })
         
     }
 
